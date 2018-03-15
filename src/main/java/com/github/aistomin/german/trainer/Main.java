52b25c9aa6f88604a3853387c94565105be76938
aistomin <andrej.istomin.ikeen@gmail.com>
2018-03-15 16:59:48 +0100

    Application skeleton was created.

---
+++ src/main/java/com/github/aistomin/german/trainer/Main.java
@@ -0,0 +1,18 @@
+package com.github.aistomin.german.trainer;
+
+/**
+ * Created by aistomin on 15.03.18.
+ * <p>
+ * Main class of the application.
+ */
+public class Main {
+
+    /**
+     * Application's entry point.
+     *
+     * @param args Arguments.
+     */
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+    }
+}
